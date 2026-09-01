80b2881d162a97efe3c2746ef04ace90de05d63d
Stone Olguin <olguinstone@gmail.com>
2026-08-31 20:59:15 -0600

    Fix indentation

---
+++ src/shiny_hunt/camera/camera.py
@@ -0,0 +1,21 @@
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+while True:
+  ret, frame = camera.read()
+
+  if not ret:
+    print("Could not read webcam")
+    break
+
+  cv2.imshow("3DS", frame)
+
+  if cv2.waitKey(1) & 0xFF == ord("q"):
+    break
+
+camera.release()
+cv2.destroyAllWindows()
+
+class Camera:
+  pass
