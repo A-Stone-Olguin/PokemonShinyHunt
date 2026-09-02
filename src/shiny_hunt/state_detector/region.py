dd9f25590236dcc5a86fd7ba4ddf3f66bf2aef4e
Stone Olguin <olguinstone@gmail.com>
2026-09-01 20:02:39 -0600

    Added region drawing

--- src/shiny_hunt/state_detector/region.py
+++ src/shiny_hunt/state_detector/region.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+import numpy as np
+import cv2
+
 @dataclass
 class Region:
   name: str
@@ -5,17 +9,34 @@ class Region:
   y: int
   width: int
   height: int
+  color: tuple = tuple(0, 255, 0) #green
 
-  def __init__(self, name, x, y, width, height):
-    self.name = name
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
-    pass
-
-  def crop(self, frame):
+  def crop(self, frame: np.ndarray) -> np.ndarray:
     return frame[
       self.y:self.y + self.height,
       self.x:self.x + self.width
     ]
+
+  def draw(
+    self,
+    frame: np.ndarray,
+    label: bool = True,
+  ) -> None:
+    cv2.rectangle(
+      frame,
+      (self.x, self.y),
+      (self.x + self.width, self.y + self.height),
+      self.color,
+      2,
+    )
+
+    if label:
+      cv2.putText(
+        frame,
+        self.name,
+        (self.x, self.y-8),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.6,
+        self.color,
+        2,
+      )
