9ae5cb680c08ce10274b85be1fa9d7ee16ba395f
Stone Olguin <olguinstone@gmail.com>
2026-09-01 21:27:09 -0600

    Revert QT commits

--- src/shiny_hunt/main.py
+++ src/shiny_hunt/main.py
@@ -1,14 +1,11 @@
 from shiny_hunt.camera.camera import Camera
 from shiny_hunt.debug.detector_debugger import DetectorDebugger
-from shiny_hunt.debug.debugger import run_debugger
 
 def main():
   camera = Camera("3DS", "./calibration.json")
   # camera.open()
-  # debugger = DetectorDebugger(camera)
-  # debugger.open()
-  run_debugger(camera)
-
+  debugger = DetectorDebugger(camera)
+  debugger.open()
 
 if __name__ == "__main__":
   main()
