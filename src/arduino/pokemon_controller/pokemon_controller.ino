00916c41ee58dcceb3c52f43460a123a2c635f38
Stone Olguin <olguinstone@gmail.com>
2026-09-02 23:26:01 -0600

    arduino progress

---
+++ src/arduino/pokemon_controller/pokemon_controller.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup() {
+  servo.attach(9);
+}
+
+void loop() {
+  // servo.write(30);
+  // delay(1000);
+
+  // servo.write(150);
+  // delay(1000);
+}
+
