cb79cf728a22a35bfa5b0c25b05ae0425b014e2d
Stone Olguin <olguinstone@gmail.com>
2026-09-04 10:16:01 -0600

    Can connect to arduino

--- src/shiny_hunt/controllers/arduino.py
+++ src/shiny_hunt/controllers/arduino.py
@@ -2,18 +2,22 @@ from shiny_hunt.hardware.arduino import ArduinoConnector
 class ArduinoController:
   def __init__(self, connector: ArduinoConnector):
     self.connector = connector
+    connector.connect()
 
-  def press(self, button: str, duration: float = 0.1):
-    self.connector.send(f"PRESS {button} {duration}")
+  def press(self, button: str):
+    self.connector.send(f"PRESS_{button}")
 
   def release(self, button: str):
-    self.connector.send(f"RELEASE {button}")
+    self.connector.send(f"RELEASE_{button}")
 
   def press_a(self):
     self.press("A")
 
   def press_b(self):
     self.press("B")
+
+  def press_start(self):
+    self.press("START")
 
   def press_up(self):
     self.press("UP")
@@ -26,3 +30,6 @@ class ArduinoController:
 
   def press_right(self):
     self.press("RIGHT")
+
+  def ping(self):
+    self.connector.send("PING")
--- src/shiny_hunt/main.py
+++ src/shiny_hunt/main.py
@@ -1,12 +1,19 @@
 from shiny_hunt.camera.camera import Camera
 from shiny_hunt.debug.detector_debugger import DetectorDebugger
+from shiny_hunt.controllers.arduino import ArduinoController, ArduinoConnector
+import time
 
 def main():
-  camera = Camera("3DS", "./calibration.json")
+  # camera = Camera("3DS", "./calibration.json")
   # camera.open()
-  debugger = DetectorDebugger(camera)
-  debugger.open()
-  # while True:
+  # camera = Camera("3DS", "./calibration.json")
+  # debugger = DetectorDebugger(camera)
+  # debugger.open()
+  arduino = ArduinoController(ArduinoConnector("/dev/ttyACM0"))
+  while True:
+    arduino.press_start()
+    time.sleep(1)
+    arduino.ping()
 
 
 if __name__ == "__main__":
